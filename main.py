f6a0925b5bcbbfbc255fe339eabe84f2feb1f927
Dhanush-R0987 <dhanush.r300285@gmail.com>
2024-06-21 02:05:58 +0530

    update: Data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from MLproject import logger
 from MLproject.pipeline.stage_01_Data_Ingestion import DataIngestionTrainingPipeline
+from MLproject.pipeline.stage_02_Data_Validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -7,6 +8,16 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n=========================================================================")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data validation stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n--------------------------------------------------------------------------")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/MLproject/config/configuration.py
+++ src/MLproject/config/configuration.py
@@ -1,6 +1,6 @@
 from MLproject.constants import *
 from MLproject.utils.common import read_yaml, create_directories
-from MLproject.entity.config_entity import (DataIngestionConfig)
+from MLproject.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 """
 Manages configuration settings for the project by reading from YAML files and creating necessary directories.
@@ -33,4 +33,16 @@ class ConfigurationManager:
 
         return data_ingestion_config
     
-    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir, 
+            all_schema = schema 
+        )
+        return data_validation_config
