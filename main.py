222820e230b26b7b2ea443278e7dde792ac57aee
Dhanush-R0987 <dhanush.r300285@gmail.com>
2024-06-21 02:16:05 +0530

    update: Data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from MLproject import logger
 from MLproject.pipeline.stage_01_Data_Ingestion import DataIngestionTrainingPipeline
 from MLproject.pipeline.stage_02_Data_Validation import DataValidationTrainingPipeline
+from MLproject.pipeline.stage_03_Data_Transformation import DataTransformationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -18,6 +20,16 @@ try:
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n--------------------------------------------------------------------------")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data transfromation stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n..........................................................................")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/MLproject/entity/config_entity.py
+++ src/MLproject/entity/config_entity.py
@@ -28,3 +28,9 @@ class DataValidationConfig:
    all_schema: dict
 
 
+@dataclass(frozen=True)
+class DataTransformationConfig:
+   root_dir: Path
+   data_path: Path
+
+
